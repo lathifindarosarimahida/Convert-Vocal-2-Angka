81cae075c81f1c2e6f632cf7a4b2e4e3b02472ac
USER <USER@DESKTOP-LSF5PJH>
2018-08-15 12:08:53 +0700

    Coba Method equals & equalsIgnoreCase

--- src/jobsheet11/CobaOperasiString.java
+++ src/jobsheet11/CobaOperasiString.java
@@ -14,6 +14,17 @@ public static void main(String[] args) {
     String y = "";
     System.out.println("isi variabel y : "+ y);
     System.out.println("y adalah kosong : "+ y.isEmpty());
+    
+    System.out.println("isi x sama dengan y : " + x.equals(y));
+    
+    String z ="Operasi";
+    System.out.println("isi variabel z : "+ z);
+    System.out.println("isi x sama dengan z (Case Sensitive):" +x.equals(z));
+    
+    String r = "operasi";
+    System.out.println("isi variabel r : "+ r);
+    System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equals (r)); 
+    System.out.println("isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
 }
 
 }
