c1f86bdb3c28901dcd09922d5f22f8b97ce9643c
USER <USER@DESKTOP-LSF5PJH>
2018-08-15 19:28:04 +0700

    Initial Commit

---
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -0,0 +1,23 @@
+package jobsheet11;
+import java.util.Scanner;
+public class ConvertVocal2Angka {  
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vocal Ke angka}\n");
+        
+    }
+    public static void main(String identitas)  {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vocal Ke angka)\n");
+      
+    }
+    public static void main(String[] args) {
+    String identitas = "Lathifinda Rosari Mahida / X RPL 2 / 19";
+        
+    tampilJudul(identitas);        
+   
+    }
+    }
+    
