b43207daeb52ac64c7f5a9fe60a7ecb209363c46
USER <USER@DESKTOP-LSF5PJH>
2018-08-15 12:04:58 +0700

    Initial Commit

---
+++ src/jobsheet11/CobaOperasiString.java
@@ -0,0 +1,9 @@
+package jobsheet11;
+import java.util.Locale;
+public class CobaOperasiString {
+public static void main(String[] args) {
+    String identitas = "Lathifinda Rosari Mahida / X RPL 2 / 19 ";
+    System.out.println("Identitas : "+ identitas);
+    
+}
+}
