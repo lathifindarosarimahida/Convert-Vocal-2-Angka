0222a689006a80aa77678b748ce45c8b4d33ffb4
USER <USER@DESKTOP-LSF5PJH>
2018-08-15 12:14:39 +0700

    Coba Method format

--- src/jobsheet11/CobaOperasiString.java
+++ src/jobsheet11/CobaOperasiString.java
@@ -63,6 +63,11 @@ public static void main(String[] args) {
     System.out.println("pecah berdasarkan huruf 'r' di variabel tclean  : ");
     for(int i=0;i<arrTr.length;i++)
         System.out.println(arrTr[i]);
+    
+    double d = 1545454000;
+    System.out.println(String.format("Uang saya %,.4f", d));
+
+    System.out.println(String.format(Locale.forLanguageTag("in-ID"), "Uang saya Rp %,.2f", d));
 }
 
 }
