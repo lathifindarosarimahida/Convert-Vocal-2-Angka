8c7e469bedb2ea337a7935991c17e4a664b58456
USER <USER@DESKTOP-LSF5PJH>
2018-08-15 12:07:17 +0700

    Coba Method & length isEmpty

--- src/jobsheet11/CobaOperasiString.java
+++ src/jobsheet11/CobaOperasiString.java
@@ -5,5 +5,15 @@ public static void main(String[] args) {
     String identitas = "Lathifinda Rosari Mahida / X RPL 2 / 19 ";
     System.out.println("Identitas : "+ identitas);
     
+    String x = "Operasi";
+    System.out.println("Isi variabel x : " + x);
+    System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+    
+    System.out.println("x adalah kosong : " + x.isEmpty());
+    
+    String y = "";
+    System.out.println("isi variabel y : "+ y);
+    System.out.println("y adalah kosong : "+ y.isEmpty());
 }
+
 }
